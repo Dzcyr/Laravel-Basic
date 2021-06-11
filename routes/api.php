984e8be9526ae9eb9144c56f7aa8d383623d9a73
佟飞 <tongfei@yinchuangyun.cn>
2021-06-11 11:18:06 +0800

    feat:添加枚举扩展包

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthorizationsController;
 use App\Http\Controllers\Api\BannersController;
 
-Route::namespace('Api')->prefix('v1')->group(function () {
+Route::prefix('v1')->group(function () {
     //Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function () {
         // 小程序 注册登录
         Route::post('weapp/authorizations', [AuthorizationsController::class, 'weappStore']);
